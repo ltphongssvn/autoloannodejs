76bdabdc1e3857b0a472988c3f8d5d4f9273f537
Thanh Phong Le <ltphongssvn@gmail.com>
2026-02-14 15:16:16 -0800

    feat: add eslint.config.js flat config for ESLint v9

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+// eslint.config.js
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2024,
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        setTimeout: 'readonly',
+        setInterval: 'readonly',
+        clearTimeout: 'readonly',
+        clearInterval: 'readonly',
+        Buffer: 'readonly',
+        URL: 'readonly',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': 'off',
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+    },
+  },
+  {
+    ignores: ['node_modules/', 'coverage/', 'dist/'],
+  },
+];
