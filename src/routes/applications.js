9a8efb46cb84f1601a6f89d78af1ee031da8701b
Thanh Phong Le <ltphongssvn@gmail.com>
2026-02-14 21:00:41 -0800

    feat: wire application sign endpoint into routes

--- src/routes/applications.js
+++ src/routes/applications.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import { authenticate } from '../middleware/auth.js';
 import { index, show, create, submit, destroy } from '../controllers/applications.js';
 import { update } from '../controllers/applicationUpdate.js';
+import { sign } from '../controllers/applicationSign.js';
 
 const router = Router();
 router.use(authenticate);
@@ -11,6 +12,7 @@ router.get('/:id', show);
 router.post('/', create);
 router.patch('/:id', update);
 router.post('/:id/submit', submit);
+router.post('/:id/sign', sign);
 router.delete('/:id', destroy);
 
 export default router;
