6a506de96cd5e0cc831ab755a5037e0d57131672
Thanh Phong Le <ltphongssvn@gmail.com>
2026-02-15 06:13:02 -0800

    fix: sequelize-cli-config use env vars for DB_HOST (Docker compatibility)

--- sequelize-cli-config.cjs
+++ sequelize-cli-config.cjs
@@ -1,16 +1,18 @@
 module.exports = {
   development: {
-    username: 'postgres',
-    password: 'postgres', //pragma: allowlist secret
-    database: 'autoloan_development',
-    host: '127.0.0.1',
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || 'postgres',
+    database: process.env.DB_NAME || 'autoloan_development',
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
   },
   test: {
-    username: 'postgres',
-    password: 'postgres', //pragma: allowlist secret
-    database: 'autoloan_test',
-    host: '127.0.0.1',
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || 'postgres',
+    database: process.env.DB_NAME || 'autoloan_test',
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
   },
   production: {
